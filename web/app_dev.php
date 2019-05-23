870e7c2807e0ece17fe54a3e80adbae1e0b4d1cf
CHANU ROMAIN <romain.chanu@univ-lyon1.fr>
2019-05-23 11:56:34 +0200

    docker ready fixes

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,9 +10,10 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+// PC DPT, VPN, docker
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('134.214.89.161','134.214.142.24', '134.214.230.137','10.42.200.33','134.214.126.65','134.214.60.107','134.214.142.133','172.29.2.53', '::1')) || php_sapi_name() === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('134.214.89.161','172.29.2.53','172.21.0.1', '::1')) || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
