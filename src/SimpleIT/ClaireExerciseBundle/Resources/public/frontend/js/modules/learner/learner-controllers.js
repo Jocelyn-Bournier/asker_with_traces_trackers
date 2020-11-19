2c1630b52a64587654ad663699b85619fbac6ed0
yepikae <remi.casado@protonmail.com>
2020-11-19 17:09:50 +0100

    Ajout de la liste de recommndations COMPER. En attente de protocole clé privée.

--- src/SimpleIT/ClaireExerciseBundle/Resources/public/frontend/js/modules/learner/learner-controllers.js
+++ src/SimpleIT/ClaireExerciseBundle/Resources/public/frontend/js/modules/learner/learner-controllers.js
@@ -5,6 +5,39 @@ learnerControllers.controller('directoryModelListController', ['$scope', '$state
         $scope.directory= DirectoryModelList.get({id: $stateParams.dirId}, function () {
         });
         //alert('hello'+$stateParams.dirId);
+        $scope.requestRecommendations = function (directory) {
+            let frameworkId = directory.framework_id;
+            $.ajax({
+                url:         "/app_dev.php/api/directories/jwt/"+frameworkId,
+                type:        "GET",
+                crossDomain: true,
+                async:       true,
+                success: function(data, textStatus){
+                    console.log(data);
+                },
+                error: function(message, textStatus){
+                    console.log(message);
+                }
+            });
+            $.ajax({
+                url:         "http://192.168.1.3:8080/jwt/recomm.php",
+                type:        "GET",
+                crossDomain: true,
+                async:       true,
+                success: function(data, textStatus){
+                    data.map(x => {
+                        x.tag    = x.tag.replace('Tag_', '');
+                        x.weight = (Math.round(x.weight * 10000)/100).toString()+' %'; 
+                    })
+                    $scope.recommendations_list = data;
+                    $scope.$apply();
+                },
+                error: function(message, textStatus){
+                    console.log(message);
+                }
+            });
+            
+        };
     }
 ]);
 learnerControllers.controller('learnerController', ['$scope', 'User', 'AttemptByExercise', 'ExerciseByModel', 'AttemptList', '$routeParams', '$location', '$stateParams',
