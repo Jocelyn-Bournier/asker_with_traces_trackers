fc0bec63a360efbe6dba406dc02bffac1cf3cac0
valentin.lachand <valentin.lachand@univ-lyon1.fr>
2022-05-31 22:45:48 +0200

    Add annoted text editor

---
+++ src/SimpleIT/ClaireExerciseBundle/Model/Resources/ExerciseModel/TextWithHoles/Model.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * This file is part of CLAIRE.
+ *
+ * CLAIRE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CLAIRE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CLAIRE. If not, see <http://www.gnu.org/licenses/>
+ */
+
+namespace SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\TextWithHoles;
+
+use JMS\Serializer\Annotation as Serializer;
+use SimpleIT\ClaireExerciseBundle\Model\Resources\ExerciseModel\Common\CommonModel;
+
+/**
+ * A short answer question model. It contains blocks of questions and a parameter to
+ * indicate if the question have to be shuffled before formatting the final
+ * version of the multiple choice.
+ *
+ * @author Baptiste Cablé <baptiste.cable@liris.cnrs.fr>
+ */
+class Model extends CommonModel
+{
+
+}
