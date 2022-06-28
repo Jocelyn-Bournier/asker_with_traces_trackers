f3ddc0237aa5697862ef250ccd8d0aa36acafb8c
Romain CHANU <romain.chanu@univ-lyon1.fr>
2022-06-28 15:31:36 +0200

    dirty one commit

--- src/SimpleIT/ClaireExerciseBundle/Form/AskerUserType.php
+++ src/SimpleIT/ClaireExerciseBundle/Form/AskerUserType.php
@@ -78,8 +78,6 @@ class AskerUserType extends AbstractType
             ->add('directories',CollectionType::class,
                 array(
                     'entry_type' => AskerUserDirectoryType::class,
-                    'allow_add' => true,
-                    'allow_delete' => true,
                     'data' => $datas,
                     'entry_options' => array(
                         'label' => false,
